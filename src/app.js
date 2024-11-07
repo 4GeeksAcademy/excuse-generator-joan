52aa85df68d97c9c87c17250e2e5d5e14924e2f9
Joan <79165234+Joangb85@users.noreply.github.com>
2024-11-07 18:24:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var who = [
+    "My dog",
+    "My donkey",
+    "One koala",
+    "The drunk guy of the neighborhood",
+    "An alien",
+    "My grandma",
+    "My evil twin"
+  ];
+  var action = [
+    " ate",
+    " broke",
+    " sold",
+    " hid",
+    " disintegrated",
+    " burned",
+    " lost"
+  ];
+  var what = [
+    " the family jewelry",
+    " my clothes",
+    " your car",
+    " your retirement savings",
+    " my homework",
+    " the treasure",
+    " your inheritance"
+  ];
+  var when = [
+    " yesterday.",
+    " on my lunchbreak.",
+    " just now.",
+    " during your breakfast.",
+    " few minutes ago.",
+    " this morning.",
+    " last week."
+  ];
+
+  var firstRandom = Math.floor(Math.random() * who.length);
+  var secondRandom = Math.floor(Math.random() * action.length);
+  var thirdRandom = Math.floor(Math.random() * what.length);
+  var fourthRandom = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[firstRandom] +
+    action[secondRandom] +
+    what[thirdRandom] +
+    when[fourthRandom];
 };
